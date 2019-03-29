cfa4e84d2aa8346e171aba43343b97aa2c694e1b
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-28 21:16:29 -0300

    criando um servidor nodeJs

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server =
+  http.createServer((request, response) => {
+
+     
+    response.write('Hello, world!');
+    response.end();
+  });
+
+server.listen(3000);
