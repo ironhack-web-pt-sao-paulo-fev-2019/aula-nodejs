ed95be7d33e70617b108dcd443baa3f848b858af
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-03-28 21:34:01 -0300

    Adicionando exemplo com 404 e 403

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const http = require('http');
 
 const server =
   http.createServer((request, response) => {
-    response.write('Hello, world!');
-    response.end();
+    console.log(`Someone has requested ${request.url}`);
+    if (request.headers.host == '127.0.0.1:3000') {
+        response.write('NAO USE O IP, tente localhost.');
+        response.statusCode = 403;
+        response.write('403 Page - Acesso proibido');
+        response.end();  
+    } else if (request.url === '/') {
+      response.write('Hello, world!');
+      response.end();
+    }
+    else {
+      response.statusCode = 404;
+      response.write('404 Page');
+      response.end();
+    }
   });
 
 server.listen(3000);
