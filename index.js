a460355a9e553bd3f19538a477fb4b51aa74b58d
Sany Chernizon <chernizon@gmail.com>
2019-03-28 21:37:32 -0300

    pagina 403 configurada

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const http = require('http');
 
 const server =
     http.createServer((request, response) => {
-        response.write('Hello, Fred!');
-        response.end();
+        console.log(`Someone has requested ${request.url}`);
+
+        if (request.headers.host == '127.0.0.1:3000'){
+            response.statusCode = 403;
+            response.write('Use o localhost!');
+            response.end();
+        }
+        else if (request.url === '/') {
+            response.write('Hello, world!');
+            response.end();
+        }
+        else {
+            response.statusCode = 404;
+            response.write('404 Page');
+            response.end();
+        }
     });
 
 server.listen(3000);
