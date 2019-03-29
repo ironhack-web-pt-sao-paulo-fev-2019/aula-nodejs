accb0fecb1c46108b47fc421b6f95f602aab5e14
kerlynh <kerlynh@gmail.com>
2019-03-28 21:34:22 -0300

    Add porta 404 - page not found

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+// const http = require('http');
+
+// const server =
+//   http.createServer((request, response) => { //cria o servidor http(envia uma solicitacao, devolve a resposta - SERVIDOR)
+//     response.write('Hello, world!'); //servidor devolve a informacao
+//     response.end();
+//   });
+
+// server.listen(3000); //servidor ativo na porta 3000
+
+// //Pode-se rodar por dois meios para apresentar o conteudo
+// //curl http://localhost:3000 (via terminal)
+// //node index.js (via navegador)
+
 const http = require('http');
 
 const server =
-  http.createServer((request, response) => { //cria o servidor http(envia uma solicitacao, devolve a resposta - SERVIDOR)
-    response.write('Hello, world!'); //servidor devolve a informacao
-    response.end();
+  http.createServer((request, response) => {
+    console.log(`Someone has requested ${request.url}`);
+    //console.log(request); -> apresenta os dados de acesso
+
+    if (request.url === '/') {
+      response.write('Hello, world!');
+      response.end();
+    }
+    else {
+      response.statusCode = 404;
+      response.write('404 Page');
+      response.end();
+    }
   });
 
-server.listen(3000); //servidor ativo na porta 3000
+server.listen(3000);
