fb97464b4e552fab12589dc4284cb21285fe106e
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-03-29 22:27:50 -0300

    Exemplo usando require para um módulo dentro do próprio projeto

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-const http = require('http');
+const server = require('./server');
 
-const server =
-  http.createServer((request, response) => {
-    console.log(`Someone has requested ${request.url}`);
-    if (request.headers.host == '127.0.0.1:3000') {
-        response.write('NAO USE O IP, tente localhost.');
-        response.statusCode = 403;
-        response.write('403 Page - Acesso proibido');
-        response.end();  
-    } else if (request.url === '/') {
-      response.write('Hello, world!');
-      response.end();
-    }
-    else {
-      response.statusCode = 404;
-      response.write('404 Page');
-      response.end();
-    }
-  });
+console.log(server);
 
 server.listen(3000);
