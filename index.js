5fb09a12da9c203242f695edb664eaa9c9658988
gui <gbrein@gmail.com>
2019-03-30 16:01:46 -0300

    hbs parte 1

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 app.use(express.static('public'));
 
+
+//HBS
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 app.get('/home', (request, response) => {
-  response.sendfile(`${__dirname}/views/home.html`);
+  response.render('home');
 });
 
 app.post('/', (request, response) => {
-  response.send(`teste! post`)
+  response.send('teste! post');
 });
 
 app.listen(3000);
