4b9b40b1621ed75a3b746cb953be50fd8d0ede7e
kerlynh <kerlynh@gmail.com>
2019-03-30 16:05:09 -0300

    instalando hbs e utilizando render

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.static('public'));
 
-//sendFile - utilizando arquivo externo com "__dirname" (variavel que identifica o caminho do arquivo)
+//Hbs
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+// // sendFile - envia arquivo
+// app.get('/home', (request, response) => {
+//   response.sendFile(`${__dirname}/views/home.html`)
+// });
+
+//add render do hbs
 app.get('/home', (request, response) => {
-  response.sendFile(`${__dirname}/views/home.html`)
+  response.render('home');
 });
 
+
+// send - envia texto
 app.post('/', (request, response) => {
   response.send(`Recebi uma requisiçao via POST!`)
 })
