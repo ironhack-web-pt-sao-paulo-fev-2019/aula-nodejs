5b6f589689b854a23706403dc037754e6775839c
Bruno <bruno@kipiai.com>
2019-03-30 16:04:17 -0300

    Implementação de HBS

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
+
+// Hbs
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
 app.use(express.static('pubic'));  
 
 app.get('/home', (request, response) => {
-    response.sendFile(`${__dirname}/views/home.html`);
+    response.render('home')
 });
 
 app.post('/', (request, response) => {
     response.send(`Recebi uma requisição via POST!`)
 });
 
-app.listen(3000);
+app.listen(3005);
 console.log('Rolou!')
