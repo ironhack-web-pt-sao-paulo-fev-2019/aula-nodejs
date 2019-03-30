456e9ae7ff69860f3176b82c58904960be98125f
Sany Chernizon <chernizon@gmail.com>
2019-03-30 11:02:56 -0300

    Pasta PUBLIC criada com arquivo .css dentro

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/home', (req,resp) => {
     resp.send(`<form action="/" method="post">
         <button type="submit">POSTAR</button>
