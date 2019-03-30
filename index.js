fcbad597955e83308fa9200099636214b031f06b
Paul <paul.divet5@gmail.com>
2019-03-30 16:30:41 -0300

    Aplicando hbs

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require('express');
-const app = express ();
-const hbs = require('hbs');
+const app = express();
 
 app.use(express.static('public'));
 
+// Hbs
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
 app.get('/home', (request, response) => {
-    response.render('home');
+    let data = {  
+        name: 'José',
+        age: 18,
+        cities: [
+            {
+                name: "Miami",
+                friends: 150,
+                special: true
+            }, 
+            { 
+                name: "Madrid",
+                friends: 185000
+            },
+            { 
+                name: "Barcelona",
+                friends: 1500
+            }
+        ]
+    }
+
+    response.render('home', data);
 });
 
-
 app.post('/', (request, response) => {
-    response.send('Recebi uma requisiçao via POST!')
+    response.send(`Recebi uma requisição via POST!`)
 });
 
 app.listen(3000);
