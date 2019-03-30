861e75e112722fc0afbbb92d720e24282f285b8d
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-30 11:33:09 -0300

    utilizando request.sendFile no get para renderizar um html

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ const app = express();
 app.use(express.static('public'))
 
 app.get('/home', (request, response) => {
-    response.send(`
-                  <html>
-                  <head>
-                      <link rel="stylesheet" href="css/styles.css" />
-                  </head>
-                  <body>
-                      <form action="/" method="post">
-                          <button type="submit">Postar dados</button>
-                      </form>
-                  </body>
-              </html>
-    `)
+    response.sendFile(`${__dirname}/views/home.html`)
 });
 
 app.post('/', (request, response) => {
