51e104b0c6244dc97eccda011a624a8a3d2137ef
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-03-30 09:37:49 -0300

    DEixando igual ao de todo mundo (retornando versão)

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const server = require('./server');
+const http = require('http');
 
-console.log(server);
+const server =
+  http.createServer((request, response) => {
+    console.log(`Someone has requested ${request.url}`);
+
+    if (request.url === '/') {
+      response.write('Hello, world!');
+      response.end();
+    }
+    else {
+      response.statusCode = 404;
+      response.write('404 Page');
+      response.end();
+    }
+  });
 
 server.listen(3000);
