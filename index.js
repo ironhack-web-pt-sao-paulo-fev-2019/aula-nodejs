81abcdc2d2e3e90b4300f38c8b0f1fd5c3993244
kerlynh <kerlynh@gmail.com>
2019-03-30 11:22:28 -0300

    utilizando .use

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 
+// metodo .use - (caminho, callback) - middlewares: funcao que sera executada dentro. Metodo adicional.
+// metodo .static - todo arquivo imutavel (ex.: imagem)
 app.use(express.static('public'));
 
 //get requisita e retorna pagina - para aparecer o retorno do post
 app.get('/home', (request, response) => {
   response.send(`
-    <form action="/" method="post">
-      <button type="submit">Postar dados</button>
-    </form>
+    <html>
+      <head>
+        <link rel="stylesheet" href="/css/styles.css" />
+      </head>
+      <body>
+        <form action="/" method="post">
+          Nome: <input name="name" value=""/>
+          <button type="submit">Postar dados</button>
+        </form>
+      </body>
+    </html>
     `)
 });
 
