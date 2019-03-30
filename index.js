f4e2e355d492131a5717208faa2457bfc80b23ac
kerlynh <kerlynh@gmail.com>
2019-03-30 10:56:43 -0300

    exemplos de get e post

--- index.js
+++ index.js
@@ -1,66 +1,24 @@
-// const http = require('http');
-
-// const server =
-//   http.createServer((request, response) => { //cria o servidor http(envia uma solicitacao, devolve a resposta - SERVIDOR)
-//     response.write('Hello, world!'); //servidor devolve a informacao
-//     response.end();
-//   });
-
-// server.listen(3000); //servidor ativo na porta 3000
-
-// //Pode-se rodar por dois meios para apresentar o conteudo
-// //curl http://localhost:3000 (via terminal)
-// //node index.js (via navegador)
-
-// const http = require('http');
-
-// const server =
-//   http.createServer((request, response) => {
-//     console.log(`Someone has requested ${request.url}`);
-//     //console.log(request); -> apresenta os dados de acesso
-
-//     if (request.url === '/') {
-//       response.write('Hello, world!');
-//       response.end();
-//     }
-//     else {
-//       response.statusCode = 404;
-//       response.write('404 Page');
-//       response.end();
-//     }
-//   });
-
-// server.listen(3000);
-
-// const http = require('http');
-
-// const server =
-//   http.createServer((request, response) => {
-//     console.log(`Someone has requested ${request.url}`);
-
-//     if (request.url === '/') {
-//       response.write('Hello, world!');
-//       response.end();
-//     }
-//     else if (request.url === '/about') {
-//       response.write('My name is Izzy');
-//       response.end();
-//     }
-//     else {
-//       response.statusCode = 404;
-//       response.write('404 Page');
-//       response.end();
-//     }
-//   });
-
-// server.listen(3000);
-
-
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send(`calma!`)
+// get requisita e retorna pagina, nao retorna o post
+// app.get('/home', (request, response) => {
+//   response.send(`Recebi uma requisiçao via GET!`)
+// })
+
+
+//get requisita e retorna pagina - para aparecer o retorno do post
+app.get('/home', (request, response) => {
+  response.send(`
+    <form action="/" method="post">
+      <button type="submit">Postar dados</button>
+    </form>
+    `)
+});
+
+//post requiita e envia dados, retorna a pagina
+app.post('/', (request, response) => {
+  response.send(`Recebi uma requisiçao via POST!`)
 })
 
 app.listen(3000);
