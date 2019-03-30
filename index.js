4c54b8de27c42b816fce115f9c70cbe91bafda85
Sany Chernizon <chernizon@gmail.com>
2019-03-30 10:26:01 -0300

    Baixamos e instalamos o express

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server =
-    http.createServer((request, response) => {
-        console.log(`Someone has requested ${request.url}`);
+app.get('/', (req,resp) => {
+    console.log('No console!')
+    resp.send('Calma Sany!')
+});
 
-        if (request.headers.host == '127.0.0.1:3000'){
-            response.statusCode = 403;
-            response.write('Use o localhost!');
-            response.end();
-        }
-        else if (request.url === '/') {
-            response.write('Hello, world!');
-            response.end();
-        }
-        else {
-            response.statusCode = 404;
-            response.write('404 Page');
-            response.end();
-        }
-    });
+app.listen(3000);
 
-server.listen(3000);
+// const server =
+//     http.createServer((request, response) => {
+//         console.log(`Someone has requested ${request.url}`);
+
+//         if (request.headers.host == '127.0.0.1:3000'){
+//             response.statusCode = 403;
+//             response.write('Use o localhost!');
+//             response.end();
+//         }
+//         else if (request.url === '/') {
+//             response.write('Hello, world!');
+//             response.end();
+//         }
+//         else {
+//             response.statusCode = 404;
+//             response.write('404 Page');
+//             response.end();
+//         }
+//     });
+
+// server.listen(3000);
