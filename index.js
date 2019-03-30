7b243019f585c9e40716775f9b69fc17fafd8795
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-30 11:05:28 -0300

    adicionando metodo static do express

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const app = express();
 
+
+app.user(static.express('public'))
+
 app.get('/home', (request, response) => {
     response.send(`
+    <link rel="stylesheet" href="/public/css/style.css" />
         <form action="/" method="post">
             <button type="submit">Postar dados</button>
         </form>
