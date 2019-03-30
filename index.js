b1c7121a86f9f6ed12633e8aa0a4a1d70897769c
Paul <paul.divet5@gmail.com>
2019-03-30 11:22:42 -0300

    Aplicando css

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ app.use(express.static('public'));
 
 app.get('/home', (request, response) => {
     response.send(`
-        <form action="/" method="post">
-            <button type="submit">Postar dados</button>
-        </form>
-    `)
+        <html>
+            <head>
+                <link rel="stylesheet" href="/css/styles.css" />
+            </head>
+            <body>
+                <form action="/" method="post">
+                    <button type="submit">Postar dados</button>
+                </form>
+            </body>
+        </html>
+    `)  
 });
 
 app.post('/', (request, response) => {
@@ -16,24 +23,3 @@ app.post('/', (request, response) => {
 });
 
 app.listen(3000);
-
-// const server =
-//   http.createServer((request, response) => {
-//     console.log(`Someone has requested ${request.url}`);
-
-//     if (request.url === '/') {
-//       response.write('Hello, world!');
-//       response.end();
-//     }
-//     else if (request.url === '/about') {
-//       response.write('My name is Izzy');
-//       response.end();
-//     }
-//     else {
-//       response.statusCode = 404;
-//       response.write('404 Page');
-//       response.end();
-//     }
-//   });
-
-// server.listen(3000);
