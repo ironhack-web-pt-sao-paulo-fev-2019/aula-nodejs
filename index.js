b58217eea20317658ebb9527eb666df8274a5a86
kerlynh <kerlynh@gmail.com>
2019-03-30 11:32:46 -0300

    utilizando .sendFile e __dirname

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const app = express();
 
-// metodo .use - (caminho, callback) - middlewares: funcao que sera executada dentro. Metodo adicional.
-// metodo .static - todo arquivo imutavel (ex.: imagem)
 app.use(express.static('public'));
 
-//get requisita e retorna pagina - para aparecer o retorno do post
+//sendFile - utilizando arquivo externo com "__dirname" (variavel que identifica o caminho do arquivo)
 app.get('/home', (request, response) => {
-  response.send(`
-    <html>
-      <head>
-        <link rel="stylesheet" href="/css/styles.css" />
-      </head>
-      <body>
-        <form action="/" method="post">
-          Nome: <input name="name" value=""/>
-          <button type="submit">Postar dados</button>
-        </form>
-      </body>
-    </html>
-    `)
+  response.sendFile(`${__dirname}/views/home.html`)
 });
 
-//post requiita e envia dados, retorna a pagina
 app.post('/', (request, response) => {
   response.send(`Recebi uma requisiçao via POST!`)
 })
