39957b82f868b70fe4f1bcaa4b93b338f786a3ee
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-30 10:56:58 -0300

    criando rotas para get e post

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
+
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`calma Sany!`)
-})
+app.get('/home', (request, response) => {
+    response.send(`
+        <form action="/" method="post">
+            <button type="submit">Postar dados</button>
+        </form>
+    `)
+});
 
-app.listen(3000)
+app.post('/', (request, response) => {
+    response.send(`Recebi uma requisição via POST!`)
+});
 
-
-
+app.listen(3000);
  
 
-// const server =
-//   http.createServer((request, response) => {
-//     console.log(`Someone has requested ${request.url}`);
-
-//     if (request.url === '/') {
-//       response.write('Hello, world!');
-//       response.end();
-//     }
-//     else if (request.url === '/about') {
-//       response.write('My name is Izzy');
-//       response.end();
-//     }
-//     else {
-//       response.statusCode = 404;
-//       response.write('404 Page');
-//       response.end();
-//     }
-//   });
-
-// server.listen(3000);
