5de74138db6415fb8c42ea797204a6f761f1e1d1
fredconti <fred@conticonti.com>
2019-03-30 10:58:13 -0300

    Fizemos um exemplo usando GET e POST!

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send(`Hello Vai rodar!!`)
+app.get('/home', (request, response) => {
+  response.send(`
+  <form action="/" method="post">
+            <button type="submit">Postar dados</button>
+        </form> `)
+});
+
+app.post('/', (request, response) => {
+  response.send(`Recebi uma requisição via POST`)
 });
 
 app.listen(3000);
