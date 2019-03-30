9a92f8ac0939c7fb942c392c06e5d66c0d0df5dc
Paul <paul.divet5@gmail.com>
2019-03-30 10:27:04 -0300

    Adicionando express e exemplo usando express

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const http = require('http');
+const express = require('express');
+const app = express ();
 
-const server =
-  http.createServer((request, response) => {
-    console.log(`Someone has requested ${request.url}`);
+app.get('/', function (request, response) {
+    response.send('Calma Sany!')
+});
 
-    if (request.url === '/') {
-      response.write('Hello, world!');
-      response.end();
-    }
-    else if (request.url === '/about') {
-      response.write('My name is Izzy');
-      response.end();
-    }
-    else {
-      response.statusCode = 404;
-      response.write('404 Page');
-      response.end();
-    }
-  });
+app.listen(3000);
 
-server.listen(3000);
+// const server =
+//   http.createServer((request, response) => {
+//     console.log(`Someone has requested ${request.url}`);
+
+//     if (request.url === '/') {
+//       response.write('Hello, world!');
+//       response.end();
+//     }
+//     else if (request.url === '/about') {
+//       response.write('My name is Izzy');
+//       response.end();
+//     }
+//     else {
+//       response.statusCode = 404;
+//       response.write('404 Page');
+//       response.end();
+//     }
+//   });
+
+// server.listen(3000);
