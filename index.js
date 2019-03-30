0895f87b92599744cf9c331b1e2384ce9fc74c03
kerlynh <kerlynh@gmail.com>
2019-03-30 10:26:19 -0300

    instalar express e criado exemplo

--- index.js
+++ index.js
@@ -12,22 +12,55 @@
 // //curl http://localhost:3000 (via terminal)
 // //node index.js (via navegador)
 
-const http = require('http');
+// const http = require('http');
 
-const server =
-  http.createServer((request, response) => {
-    console.log(`Someone has requested ${request.url}`);
-    //console.log(request); -> apresenta os dados de acesso
+// const server =
+//   http.createServer((request, response) => {
+//     console.log(`Someone has requested ${request.url}`);
+//     //console.log(request); -> apresenta os dados de acesso
 
-    if (request.url === '/') {
-      response.write('Hello, world!');
-      response.end();
-    }
-    else {
-      response.statusCode = 404;
-      response.write('404 Page');
-      response.end();
-    }
-  });
+//     if (request.url === '/') {
+//       response.write('Hello, world!');
+//       response.end();
+//     }
+//     else {
+//       response.statusCode = 404;
+//       response.write('404 Page');
+//       response.end();
+//     }
+//   });
 
-server.listen(3000);
+// server.listen(3000);
+
+// const http = require('http');
+
+// const server =
+//   http.createServer((request, response) => {
+//     console.log(`Someone has requested ${request.url}`);
+
+//     if (request.url === '/') {
+//       response.write('Hello, world!');
+//       response.end();
+//     }
+//     else if (request.url === '/about') {
+//       response.write('My name is Izzy');
+//       response.end();
+//     }
+//     else {
+//       response.statusCode = 404;
+//       response.write('404 Page');
+//       response.end();
+//     }
+//   });
+
+// server.listen(3000);
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send(`calma!`)
+})
+
+app.listen(3000);
